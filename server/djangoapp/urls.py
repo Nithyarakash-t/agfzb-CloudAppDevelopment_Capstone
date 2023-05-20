23eebdbfa9a8281e60036d24062f3163ef2339bf
nithyaprakash-t <nithyaprakash.t@gmail.com>
2023-05-20 14:37:54 -0400

    Module 4 incomplete

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     # view refers to the view function
     # name the URL
     # path(route='', view=views.index, name='index'),
-        path(route='', view=views.get_dealerships, name='index'),
+    path(route='', view=views.get_dealerships, name='index'),
 
     # path for about view
     path(route='about/', view=views.about, name='about'),
